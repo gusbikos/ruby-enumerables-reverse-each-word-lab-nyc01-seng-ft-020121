92faea2f60a067bf67e68092e6988ca067395930
Gus Bikos <gusbikos@gmail.com>
2021-01-20 06:22:07 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,9 @@ sentence = "Hello there, how are you?"
 
 def reverse_each_word(sentence)
 
-  sentence = str.split(" ")
-  sentence.collect do |word|
+  array = sentence.split(" ")
+  array.collect do |word|
     word.reverse!
   end
-  sentence.join(" ")
+  array.join(" ")
 end
